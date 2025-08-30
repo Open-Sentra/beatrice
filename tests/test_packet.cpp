912241f2c9e7f89b5ff3dc2918f23abc4faf30f8
yasinldev <ozkayayasin964@gmail.com>
2025-08-31 02:35:33 +0300

    Update: errors resolved

--- tests/test_packet.cpp
+++ tests/test_packet.cpp
@@ -8,8 +8,8 @@ TEST(PacketTest, PacketCreation) {
 
 TEST(PacketTest, PacketSize) {
     std::vector<uint8_t> data = {1, 2, 3, 4, 5};
-    auto dataPtr = std::make_shared<const uint8_t[]>(data.size());
-    std::copy(data.begin(), data.end(), const_cast<uint8_t*>(dataPtr.get()));
+    auto dataPtr = std::make_shared<uint8_t[]>(data.size());
+    std::copy(data.begin(), data.end(), dataPtr.get());
     beatrice::Packet packet(dataPtr, data.size());
     EXPECT_EQ(packet.size(), 5);
 } 
